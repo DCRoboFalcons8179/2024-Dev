0eb362a9f2daf2d99793a2dffff971becc242465
MADMAN-Modding <mad@kmdtech.net>
2024-02-04 16:59:28 -0500

    Fixed the camera left method a bit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,7 +108,7 @@ public class RobotContainer {
         approachTag.whileTrue(new ApproachTag(s_Swerve, limelight, 2, 20, 4.5, 2, 0.15, 6, 10, new Translation2d(0, 2), 0, false));
         bButton.onTrue(new InstantCommand(() -> shooter.setShooterSpeed(0.5)).andThen(() -> shooter.setBeaterBarSpeed(0.1)));
         bButton.onFalse(new InstantCommand(() -> shooter.setShooterSpeed(0)).andThen(() -> shooter.setBeaterBarSpeed(0)));
-        dualShockPovUp.onTrue(new InstantCommand(() -> cameras.cameraControllerLeft()));
+        dualShockPovUp.onTrue(new InstantCommand(() -> cameras.cameraControllerLeft(1)));
 
         /*bButton.onTrue(new InstantCommand(() -> shooter.setBeaterBarSpeed(0.5)));
         bButton.onFalse(new InstantCommand(() -> shooter.setBeaterBarSpeed(0)));
--- src/main/java/frc/robot/subsystems/Cameras.java
+++ src/main/java/frc/robot/subsystems/Cameras.java
@@ -3,18 +3,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Cameras {
-    public int cameraControllerLeft() {
-
-
-        // SmartDashboard.putNumber("test", 40);
+    public void cameraControllerLeft(int add) {
 
         double leftCam = SmartDashboard.getNumber("Left Camera Value", 0);
 
-        SmartDashboard.putNumber("Left Camera Value", 4);
-
-        int leftCamInt = (int) leftCam++;
-
-        return leftCamInt;
+        SmartDashboard.putNumber("Left Camera Value", leftCam++);
 
     }
 }
