dc0fc5f7b2004e016047484f3abc611d3dd099d0
MADMAN-Modding <mad@kmdtech.net>
2024-04-02 17:25:15 -0400

    Gradle fix

---
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems;
+
+import java.util.Objects;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PhotonVision extends SubsystemBase {
+
+    private PhotonCamera camera = new PhotonCamera("photonvision");
+    private boolean seesObject = false;
+    private PhotonTrackedTarget object;
+
+    @Override
+    public void periodic() {
+        var toSet = camera.getLatestResult().getBestTarget();
+        if (Objects.isNull(toSet)) {
+            seesObject = true;
+            object = toSet;
+        } else {
+            seesObject = false;
+        }
+    }
+
+    public boolean seesObject() {
+        return seesObject;
+    }
+
+    /**
+     * should probably check if you see an object
+     */
+    public double getRingOffsetX() {
+        return object.getYaw();
+    }
+
+    /**
+     * should probably check if you see an object
+     */
+    public double getRingOffsetY() {
+        return object.getPitch();
+    }
+
+}
